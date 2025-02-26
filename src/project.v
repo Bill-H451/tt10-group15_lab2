ba98af1273e64cbbad410edccc83e4ef886e1007
0nsemir0 <yc5057@nyu.edu>
2025-02-26 14:48:16 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -10,32 +10,30 @@ module tt_um_pe (en, In, C);
     input [15:0] In;
     output reg [7:0] C; 
 
-    
-    always @(en,In) 
-        begin
-            if(en==1)
-                begin
-                    if (In[15]) C = 8'd15;
-                        else if (In[14]) C = 8'd14;
-                        else if (In[13]) C = 8'd13;
-                        else if (In[12]) C = 8'd12;
-                        else if (In[11]) C = 8'd11;
-                        else if (In[10]) C = 8'd10;
-                        else if (In[9])  C = 8'd9;
-                        else if (In[8])  C = 8'd8;
-                        else if (In[7])  C = 8'd7;
-                        else if (In[6])  C = 8'd6;
-                        else if (In[5])  C = 8'd5;
-                        else if (In[4])  C = 8'd4;
-                        else if (In[3])  C = 8'd3;
-                        else if (In[2])  C = 8'd2;
-                        else if (In[1])  C = 8'd1;
-                        else if (In[0])  C = 8'd0;
-                        else C = 8'b1111_0000; // Special case when all bits are zero
-            end
-        end 
-    
+    always @(*) // Use @(*) for combinational logic
+    begin
+        if (en) begin
+            if (In[15])      C <= 8'd15;
+            else if (In[14]) C <= 8'd14;
+            else if (In[13]) C <= 8'd13;
+            else if (In[12]) C <= 8'd12;
+            else if (In[11]) C <= 8'd11;
+            else if (In[10]) C <= 8'd10;
+            else if (In[9])  C <= 8'd9;
+            else if (In[8])  C <= 8'd8;
+            else if (In[7])  C <= 8'd7;
+            else if (In[6])  C <= 8'd6;
+            else if (In[5])  C <= 8'd5;
+            else if (In[4])  C <= 8'd4;
+            else if (In[3])  C <= 8'd3;
+            else if (In[2])  C <= 8'd2;
+            else if (In[1])  C <= 8'd1;
+            else if (In[0])  C <= 8'd0;
+            else             C <= 8'b1111_0000; // Special case when all bits are 0
+        end
+    end
 endmodule
+
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
